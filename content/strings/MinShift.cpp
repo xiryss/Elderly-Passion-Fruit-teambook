6acaa529bcb7f02c09631b0e25e96f4d41fa2cc1
Iura Pustovalov <chereshnick@yandex.ru>
2022-12-04 03:37:31 +0300

    added things

---
+++ content/strings/MinShift.cpp
@@ -0,0 +1,24 @@
+/**
+ * Author: Iurii Pustovalov 
+ * Date: 2022-11-08
+ * Description: Calculates min-cyclic-shift of s, Duval decomposition
+ * Time: O(n)
+ */
+string minshift(string s) {
+    int i = 0, ans = 0;
+    s += s;
+    int n = s.size();
+    while (i < n / 2) {
+        ans = i;
+        int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) {
+            if (s[k] < s[j]) k = i;
+            else ++k;
+            ++j;
+        }
+        while (i <= k) {
+            i += j - k;
+        }
+    }
+    return s.substr(ans, n / 2);
+}
